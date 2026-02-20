b63031b38a2c0c20d9356deba205c41ff87e0907
OneBoy <git@git.com>
2026-02-20 23:32:03 +0530

    electro update

---
+++ class-12/electrostatics/assets/js/math.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+script.async = true;
+document.head.appendChild(script);
